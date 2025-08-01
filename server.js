4b47f909caecc4c029749aef9ab3e0af5ff5b97b
Fleurea-Dispatcher-Developpment-Company <fleureadispatcher@gmail.com>
2025-08-01 18:21:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 const url = "fleuread.onrender.com";
-console.log(`Le serveur de Fleuread fonctionne à l'adresse ${url}:${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Le serveur de Fleuread fonctionne à l'adresse ${url}:${PORT}`);
+});
+
