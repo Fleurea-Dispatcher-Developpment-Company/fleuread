199723686694666fd6bc282a864b848189f81f12
Fleurea-Dispatcher-Developpment-Company <fleureadispatcher@gmail.com>
2025-08-05 15:51:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -376,7 +376,7 @@ async function checkNextHour (time) {
  }
 }
 
-function formatHour (time, offset) {
+function formatHour (time, offset || 0) {
   console.log("Entrée :", time, "+",offset);
   console.log(offset);
   const now = new Date(time.getTime() + offset * 60000);
