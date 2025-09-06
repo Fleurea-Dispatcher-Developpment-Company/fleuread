4b7cae7504e53dfe77e804bd946a22298e33cbbe
Fleurea-Dispatcher-Developpment-Company <fleureadispatcher@gmail.com>
2025-09-06 18:30:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -670,7 +670,7 @@ app.post('/editbenne', async (req, res) => {
       res.send("Édition enregistrée avec succès !");
       console.log("AVANT");
       setHistorique (sessions[thisid].id, value_eq, "1", "bennes", value_toupd, toupd); // Affectation à l'historique de la benne
-      setHistorique (value_eq, sessions[thisid].id, "1", "bennes", value_toupd, toupd); // Affectation à l'historique de l'actionneur
+      setHistorique (value_eq, sessions[thisid].id, "1", "comptes", value_toupd, toupd); // Affectation à l'historique de l'actionneur
       console.log("APRÈS");
       const {latitude, longitude} = await getAdresseBenneEdit(value_eq);
       console.log("Entrée adresse benne", latitude, "*", longitude);
