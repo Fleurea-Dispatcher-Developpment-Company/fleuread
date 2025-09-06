5c8959119672ce9e0316ea926cc62f0727abe9ac
Fleurea-Dispatcher-Developpment-Company <fleureadispatcher@gmail.com>
2025-09-06 11:46:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const StaticMaps = require('staticmaps');
 const fs = require('fs');
 const QRCode = require('qrcode');
 const {PDFDocument, rgb, StandardFonts} = require('pdf-lib');
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name : process.env.CLOUDINARY_NAME,
+  api_key : process.env.CLOUDINARY_KEY,
+  api_secret : process.env.CLOUDINARY_SECRET,
+  secure:true
+});
 
 // On gère tous les app.something
 const app = express();
