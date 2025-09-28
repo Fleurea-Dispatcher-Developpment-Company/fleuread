0a6220a6976621280d182d63589d1491f248a60c
Fleurea-Dispatcher-Developpment-Company <fleureadispatcher@gmail.com>
2025-09-28 08:41:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1089,17 +1089,17 @@ async function pdfWithQr(id, filePath) {
 
   const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  const fontSizeHeader = 50;
+  const fontSizeHeader = 40;
   const headerText = `Benne n°${id}`;
   const textWidth = font.widthOfTextAtSize(headerText, fontSizeHeader);
   const xCenter = (page.getWidth() - textWidth) / 2;
   
   page.drawText(headerText,{
-    x:xCenter, y:790, size:fontSizeHeader, font, color:rgb(0,0,0)
+    x:xCenter, y:780, size:fontSizeHeader, font, color:rgb(0,0,0)
   });
 
-  page.drawRectangle({x:(595-400)/2, y:350, width:400, height:400, borderColor:rgb(0.8,0,0), borderWidth:4}); // Grand cadre
-  page.drawRectangle({x:(595-390)/2, y:355, width:390, height:390, borderColor:rgb(1,0,0), borderWidth:2}); // Petit cadre
+  page.drawRectangle({x:(595-400)/2, y:320, width:400, height:400, borderColor:rgb(0.8,0,0), borderWidth:4}); // Grand cadre
+  page.drawRectangle({x:(595-390)/2, y:325, width:390, height:390, borderColor:rgb(1,0,0), borderWidth:2}); // Petit cadre
 const qrImage = await pdfDoc.embedPng(base64Data);
   const qrDims = qrImage.scale(1);
   page.drawImage(qrImage, {x:200 + (195 - qrDims.width) / 2, y : 400, width:qrDims.width, height:qrDims.height});
@@ -1108,7 +1108,7 @@ const numberText = String(id);
 const numberWidth = font.widthOfTextAtSize(numberText, fontSizeNumber);
 page.drawText(numberText, {
   x: 200 + (195 - numberWidth) / 2,
-  y: 365,
+  y: 355,
   size: fontSizeNumber,
   font,
   color: rgb(0.7, 0, 0), // rouge foncé
